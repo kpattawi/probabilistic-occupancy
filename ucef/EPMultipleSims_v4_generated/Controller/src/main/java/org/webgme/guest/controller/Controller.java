151cdc9bda18daa306b9fc496df7f64beebe1922
Kaleb Pattawi <72418535+kpattawi@users.noreply.github.com>
2021-05-26 17:52:37 -0700

    DEBUG: indentation error

--- ucef/EPMultipleSims_v4_generated/Controller/src/main/java/org/webgme/guest/controller/Controller.java
+++ ucef/EPMultipleSims_v4_generated/Controller/src/main/java/org/webgme/guest/controller/Controller.java
@@ -428,40 +428,40 @@ public class Controller extends ControllerBase {
               
               if (hour%1 == 0){
                 try {
-                    sblock= String.valueOf((int)hour);
-                    sday = String.valueOf(day);
-                    dataStringOptHeat = sblock;
-                    dataStringOptCool = sblock;
-                    System.out.println("sblock:" +sblock);
-                    System.out.println("sday:" +sday);
-                    System.out.println("zonetemp string" +String.valueOf(zoneTemps[i]));
+                  sblock= String.valueOf((int)hour);
+                  sday = String.valueOf(day);
+                  dataStringOptHeat = sblock;
+                  dataStringOptCool = sblock;
+                  System.out.println("sblock:" +sblock);
+                  System.out.println("sday:" +sday);
+                  System.out.println("zonetemp string" +String.valueOf(zoneTemps[i]));
 
-                    // Process p = Runtime.getRuntime().exec("python ./energyOpt.py " +sday +" "+sblock +" "+ String.valueOf(zoneTemps[i])); // 4 hr block method
-                    System.out.println("Run python code...");
-                    Process p = Runtime.getRuntime().exec("python3 "+PATH_TO_probabilistic_occupancy+"optimization/prob_occ.py " +sday +" " +sblock +" "+ String.valueOf(zoneTemps[i])); // 1 timestep method
-    
-                    BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
-                    System.out.println("Here is the result");
+                  // Process p = Runtime.getRuntime().exec("python ./energyOpt.py " +sday +" "+sblock +" "+ String.valueOf(zoneTemps[i])); // 4 hr block method
+                  System.out.println("Run python code...");
+                  Process p = Runtime.getRuntime().exec("python3 "+PATH_TO_probabilistic_occupancy+"optimization/prob_occ.py " +sday +" " +sblock +" "+ String.valueOf(zoneTemps[i])); // 1 timestep method
+  
+                  BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                  System.out.println("Here is the result");
 
-                    // T is indoor Temp, P is price, O is outdoor T, S is solar rad
-            
-                    while ((s = stdInput.readLine()) != null) {
-                        System.out.println(s);
-                        if (startHeat == true) {
-                            dataStringOptHeat = dataStringOpt + separatorOpt + s;
-                        }		
-                        if (startCool == true) {
-                            dataStringOptCool = dataStringOptT + separatorOpt + s;
-                        }
-                        if (s .equals("heating setpoint")){
-                            startHeat = true;
-                        }
-                        if (s .equals("cooling setpoint")){
-                            startCool = true;
-                        }
-                        // System.out.println(dataString);
+                  // T is indoor Temp, P is price, O is outdoor T, S is solar rad
+          
+                  while ((s = stdInput.readLine()) != null) {
+                      System.out.println(s);
+                      if (startHeat == true) {
+                          dataStringOptHeat = dataStringOpt + separatorOpt + s;
+                      }		
+                      if (startCool == true) {
+                          dataStringOptCool = dataStringOptT + separatorOpt + s;
                       }
-                  } catch (IOException e) {
+                      if (s .equals("heating setpoint")){
+                          startHeat = true;
+                      }
+                      if (s .equals("cooling setpoint")){
+                          startCool = true;
+                      }
+                      // System.out.println(dataString);
+                    }
+                } catch (IOException e) {
                       e.printStackTrace();
                   }
                   String vars[] = dataStringOptHeat.split(separatorOpt);
@@ -492,27 +492,28 @@ public class Controller extends ControllerBase {
                 p=p+1;
                 System.out.println("p"+String.valueOf(p));
               }
-
-              // For Cooling 1 degree under Cooling setpoint:
-              if (zoneTemps[i] >= coolTemps[i]-.1){ // first check if going to exit maximum band
-                  fuzzy_cool = -1;
-              }else if (zoneTemps[i] <= coolTemps[i]-1.1){
-                  fuzzy_cool = 1;
-              }
-              coolTemps[i] = coolTemps[i] - 0.6 +fuzzy_cool*OFFSET;   // -0.6 so that oscillates 0.1-1.1 degree under cooling setpoint
-
-              // For Heating 1 degree under Heating setpoint:
-              if (zoneTemps[i] <= heatTemps[i]+.1){ // first check if going to exit minimum band
-                  fuzzy_heat = 1;
-              }else if (zoneTemps[i] >= heatTemps[i]+1.1){
-                  fuzzy_heat = -1;
-              }
-              heatTemps[i] = heatTemps[i] + 0.6 +fuzzy_heat*OFFSET;  // +0.6 so that oscillates 0.1-1.1 degree above heating setpoint
-              // end fuzzy -------------------------
-              coolTemps[i] = 30.2; // do this for now to avoid turning on AC
-              System.out.println("heatTemp after fuzzy:"+String.valueOf(heatTemps[i]));
-              System.out.println("coolTemp after fuZzy:"+String.valueOf(coolTemps[i]));
             }
+
+            // For Cooling 1 degree under Cooling setpoint:
+            if (zoneTemps[i] >= coolTemps[i]-.1){ // first check if going to exit maximum band
+                fuzzy_cool = -1;
+            }else if (zoneTemps[i] <= coolTemps[i]-1.1){
+                fuzzy_cool = 1;
+            }
+            coolTemps[i] = coolTemps[i] - 0.6 +fuzzy_cool*OFFSET;   // -0.6 so that oscillates 0.1-1.1 degree under cooling setpoint
+
+            // For Heating 1 degree under Heating setpoint:
+            if (zoneTemps[i] <= heatTemps[i]+.1){ // first check if going to exit minimum band
+                fuzzy_heat = 1;
+            }else if (zoneTemps[i] >= heatTemps[i]+1.1){
+                fuzzy_heat = -1;
+            }
+            heatTemps[i] = heatTemps[i] + 0.6 +fuzzy_heat*OFFSET;  // +0.6 so that oscillates 0.1-1.1 degree above heating setpoint
+            // end fuzzy -------------------------
+            coolTemps[i] = 30.2; // do this for now to avoid turning on AC
+            System.out.println("heatTemp after fuzzy:"+String.valueOf(heatTemps[i]));
+            System.out.println("coolTemp after fuZzy:"+String.valueOf(coolTemps[i]));
+            
           }
           //-------------------------------------------------------------------------------------------------
 
